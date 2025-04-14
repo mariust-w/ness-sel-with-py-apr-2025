885a2e83af7db69c694181916221e37184eea577
Testuser <duraiatpeers@gmail.com>
2025-04-14 09:24:46 +0530

    new commit apr14

--- Quiz/quiz.md
+++ Quiz/quiz.md
@@ -2,6 +2,11 @@ Quiz1:
 Git, Python Basics - 10 MCQs
 https://quizizz.com/pro/join?gc=46683684
 
+
 Optional Quiz for practice:
 Python - 28 MCQs
 https://forms.gle/85eVkXFYARn1njpbA
+
+Quiz2
+Git, Selenium Basics - 15 MCQs
+https://quizizz.com/pro/join?gc=59056804
---
+++ day4/dummy.py
@@ -0,0 +1,14 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+#
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+# import pytest
+#
+#
+# def test_googletest():
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#
+#     # navigate to the app url
+#     driver.get("https://google.com")
+
